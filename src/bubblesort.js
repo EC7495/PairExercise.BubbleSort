79423806854edee652e1b240ddc7a8e2e81c6f46
Erick <ecanals07@gmail.com>
2020-02-18 11:54:09 -0500

    cleaning up

--- src/bubblesort.js
+++ src/bubblesort.js
@@ -1,16 +1,12 @@
 function bubbleSort(array, index = array.length - 1) {
   let i = 0;
   let j = 1;
-  let last = array.length - 1;
 
-  // while (last > 0) {
   if (index === 0) return array;
   else {
     while (index >= j) {
       if (array[i] > array[j]) {
-        let temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+        [array[i], array[j]] = [array[j], array[i]];
       }
 
       i += 1;
@@ -19,13 +15,6 @@ function bubbleSort(array, index = array.length - 1) {
 
     return bubbleSort(array, index - 1);
   }
-
-  //   i = 0;
-  //   j = 1;
-  //   last -= 1;
-  // }
-
-  return array;
 }
 
 module.exports = bubbleSort;
