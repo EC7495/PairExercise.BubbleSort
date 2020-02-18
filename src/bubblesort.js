5462dbd48ecada8c3e11a33b830ba74b77072e37
Erick <ecanals07@gmail.com>
2020-02-18 11:51:34 -0500

    implemented using recursion

--- src/bubblesort.js
+++ src/bubblesort.js
@@ -1,3 +1,31 @@
-function bubbleSort() {
-  return [1, 2, 3];
+function bubbleSort(array, index = array.length - 1) {
+  let i = 0;
+  let j = 1;
+  let last = array.length - 1;
+
+  // while (last > 0) {
+  if (index === 0) return array;
+  else {
+    while (index >= j) {
+      if (array[i] > array[j]) {
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+
+      i += 1;
+      j += 1;
+    }
+
+    return bubbleSort(array, index - 1);
+  }
+
+  //   i = 0;
+  //   j = 1;
+  //   last -= 1;
+  // }
+
+  return array;
 }
+
+module.exports = bubbleSort;
--- test/bubblesort.spec.js
+++ test/bubblesort.spec.js
@@ -1,10 +1,11 @@
 const { expect } = require('chai');
+const bubbleSort = require('../src/bubblesort');
 
 const unsorted = [2, 4, 5, 7, 3, 8, 6, 1];
 const sorted = [1, 2, 3, 4, 5, 6, 7, 8];
 
 describe('BubbleSort', () => {
   it('should sort an unsorted array', () => {
-    expect(bubbleSort(unsorted)).to.be(sorted);
+    expect(bubbleSort(unsorted)).to.be.deep.equal(sorted);
   });
 });
